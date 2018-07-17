2ffd39c809e4dd1c2d13fed7bc38742bdeb021d9
Michael Li <michael.li48.ml@gmail.com>
2018-07-17 13:27:55 -0700

    Delete CritteringStarter.java

--- WebCritterer/src/com/webcritterer/CritteringStarter.java
+++
@@ -1,9 +0,0 @@
-package com.webcritterer;
-
-public class CritteringStarter {
-
-    public static void main(String[] args) {
-        Critterer critterer = new Critterer(); //initializes Critterer class
-        critterer.load("http://uci.edu"); // INSERT SEED URL here
-    }
-}

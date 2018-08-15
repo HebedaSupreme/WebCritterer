2cc44bb562fd95ab1dd7f0770fcf9ad41fe450b4
Hebeda_Supreme <hebedasupreme9868@gmail.com>
2018-08-15 15:19:30 -0700

    remove --help typo

--- src/com/webcritterer/CrittererLaunchpad.java
+++ src/com/webcritterer/CrittererLaunchpad.java
@@ -17,7 +17,7 @@ public class CrittererLaunchpad {
     public void launchpad() {
         if (args.length == 0 || args[0].equals("--help")) {
             System.out.println("Usage: " + usageMsg);
-            return;g
+            return;
         }
         long maxPagesGoingTo = 0;
         boolean pagesLimiter = false;
